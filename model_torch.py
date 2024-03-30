333c6b35d6ea5bfec052fe1a301fd39f90824bd0
Marinescu Alexandru-Ion <8056864+Zerseu@users.noreply.github.com>
2024-03-30 20:04:56 +0200

    Full port of keras to torch... Testing...

--- model_torch.py
+++ model_torch.py
@@ -21,16 +21,16 @@ class TorchModule(Module):
     def __init__(self, vocabulary_size: int, hidden_size: int):
         super(TorchModule, self).__init__()
         self.embedding = torch.nn.Embedding(num_embeddings=vocabulary_size, embedding_dim=hidden_size)
-        self.spatial_dropout = torch.nn.Dropout(p=0.25)
+        self.dropout = torch.nn.Dropout(p=0.25)
         self.lstm = torch.nn.LSTM(input_size=hidden_size, hidden_size=hidden_size, batch_first=True)
-        self.dense = torch.nn.Linear(in_features=hidden_size, out_features=vocabulary_size)
+        self.linear = torch.nn.Linear(in_features=hidden_size, out_features=vocabulary_size)
 
-    def forward(self, x: torch.LongTensor) -> torch.LongTensor:
+    def forward(self, x: torch.LongTensor) -> torch.FloatTensor:
         x = self.embedding(x)
-        x = self.spatial_dropout(x)
+        x = self.dropout(x)
         x, _ = self.lstm(x)
         x = x[:, -1, :]
-        x = self.dense(x)
+        x = self.linear(x)
         return x
 
 
@@ -65,12 +65,36 @@ class Model:
             cpu_count = multiprocessing.cpu_count() - 4
             train_loader = DataLoader(dataset=d_trn, batch_size=cfg[kind]['batch_size'], num_workers=cpu_count, shuffle=True)
             valid_loader = DataLoader(dataset=d_val, batch_size=cfg[kind]['batch_size'], num_workers=cpu_count)
-            Model.__train_model__(model, train_loader, valid_loader, loss_function, optimizer)
+            csv_logger = os.path.join(crt_dir, kind + '_log.csv')
+
+            Model.__train_model__(model,
+                                  train_loader,
+                                  valid_loader,
+                                  loss_function,
+                                  optimizer,
+                                  csv_logger,
+                                  cfg[kind]['number_of_epochs'])
             torch.save(model.state_dict(), os.path.join(crt_dir, kind + '_model.torch'))
 
         if mode == 'test' and not os.path.exists(os.path.join(crt_dir, kind + '_output.txt')):
             model = TorchModule(vocabulary_size, cfg[kind]['hidden_size'])
             model.load_state_dict(torch.load(os.path.join(crt_dir, kind + '_model.torch')))
+
+            number_of_steps = 0
+            for key in cfg:
+                number_of_steps = max(number_of_steps, cfg[key]['number_of_steps'])
+            with open(os.path.join(crt_dir, kind + '_input.txt'), 'rt') as file:
+                inception = file.read().split()[:number_of_steps]
+            sentence_ids = [map_direct[element] for element in inception]
+            sentence = inception
+            for _ in tqdm(range(predictions)):
+                i = sentence_ids[-number_of_steps:]
+                o = Model.__temp_predict__(model, i, cfg[kind]['temperature'])
+                sentence_ids.append(o)
+                sentence.append(map_reverse[o])
+            sentence = ' '.join(sentence)
+            with open(os.path.join(crt_dir, kind + '_output.txt'), 'wt') as file:
+                file.write(sentence)
 
     @staticmethod
     def __read_sentences__(pth: str) -> [[str]]:
@@ -128,44 +152,43 @@ class Model:
                         valid_loader: DataLoader,
                         loss_function: CrossEntropyLoss,
                         optimizer: Adam,
+                        csv_logger: str,
                         num_epochs: int = 10):
-        for epoch in range(num_epochs):
-            model.train()
-            total_train_loss = 0
-            for inputs, labels in tqdm(train_loader):
-                optimizer.zero_grad()
-                outputs = model(inputs)
-                loss = loss_function(outputs, labels)
-                loss.backward()
-                optimizer.step()
-                total_train_loss += loss.item()
-            sys.stderr.flush()
-
-            model.eval()
-            total_valid_loss = 0
-            with torch.no_grad():
-                for inputs, labels in tqdm(valid_loader):
+        with open(csv_logger, 'wt') as log:
+            log.write('epoch,train_loss,validation_loss\n')
+            for epoch in tqdm(range(num_epochs)):
+                model.train()
+                total_train_loss = 0
+                for inputs, labels in train_loader:
+                    optimizer.zero_grad()
                     outputs = model(inputs)
                     loss = loss_function(outputs, labels)
-                    total_valid_loss += loss.item()
-            sys.stderr.flush()
+                    loss.backward()
+                    optimizer.step()
+                    total_train_loss += loss.item()
 
-            avg_train_loss = total_train_loss / len(train_loader)
-            avg_valid_loss = total_valid_loss / len(valid_loader)
-            print(f'Epoch {epoch + 1}/{num_epochs}, Train Loss: {avg_train_loss:.4f}, Validation Loss: {avg_valid_loss:.4f}')
+                model.eval()
+                total_valid_loss = 0
+                with torch.no_grad():
+                    for inputs, labels in valid_loader:
+                        outputs = model(inputs)
+                        loss = loss_function(outputs, labels)
+                        total_valid_loss += loss.item()
+
+                avg_train_loss = total_train_loss / len(train_loader)
+                avg_valid_loss = total_valid_loss / len(valid_loader)
+                log.write(f'{epoch + 1},{avg_train_loss:.4f},{avg_valid_loss:.4f}\n')
 
     @staticmethod
-    def __temp_sample__(preds, temp=1.0):
-        preds = np.asarray(preds).astype('float64')
-        preds = np.log(preds + 1e-9) / temp
-        exp_preds = np.exp(preds)
-        preds = exp_preds / np.sum(exp_preds)
-        probs = np.random.multinomial(1, preds, 1)
+    def __temp_sample__(preds: torch.FloatTensor, temp: float = 1.0) -> int:
+        preds = preds.detach().numpy().astype(np.float64)[0]
+        preds = np.exp(preds / temp)
+        probs = np.random.multinomial(1, preds / np.sum(preds), 1)
         return np.argmax(probs)
 
     @staticmethod
-    def __temp_predict__(model, seq, temp=1.0):
-        preds = model.predict(seq, verbose=0)[0]
+    def __temp_predict__(model: Module, seq: list[int], temp: float = 1.0) -> int:
+        preds = model(torch.from_numpy(np.array(seq, dtype=int).reshape((1, -1))).long())
         return Model.__temp_sample__(preds, temp)
 
 
