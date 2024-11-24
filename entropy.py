fc6578d77103a69a852d95cab67b5ac1b9f63eb7
Marinescu Alexandru-Ion <cc_plusplus@yahoo.com>
2024-11-24 19:49:11 +0200

    Re-synced requirements.txt...

--- entropy.py
+++ entropy.py
@@ -82,10 +82,12 @@ def composer_entropy(composer: str, instruments: [str]) -> (float, float):
     x_values = list(x_values)
     y_values = list(y_values)
 
-    pth_plot = os.path.join(crt_dir, 'entropy_plot_measured.png')
     dpi = 72
-    fig_width = 3000
-    fig_height = 750
+    fig_width = 1000
+    fig_height = 500
+    ln_width = 2
+
+    pth_plot = os.path.join(crt_dir, 'entropy_plot_measured.png')
     plt.figure(figsize=(fig_width / dpi, fig_height / dpi), dpi=dpi)
     plt.bar(seq_len_str, x_values, label='Real Entropy')
     plt.bar(seq_len_str, y_values, bottom=x_values, label='Noise Entropy')
@@ -97,14 +99,12 @@ def composer_entropy(composer: str, instruments: [str]) -> (float, float):
     plt.close('all')
 
     pth_plot = os.path.join(crt_dir, 'entropy_plot_interpolated.png')
-    dpi = 72
-    fig_width = 3000
-    fig_height = 750
     plt.figure(figsize=(fig_width / dpi, fig_height / dpi), dpi=dpi)
     interp = interp1d(seq_len_int, x_values, kind='linear')
     x_interp = np.linspace(min(seq_len_int), max(seq_len_int), 500).tolist()
     y_interp = [interp(x) for x in x_interp]
-    plt.plot(x_interp, y_interp, linestyle='-', color='blue', label='Interpolated Entropy')
+    plt.plot(x_interp, y_interp, linestyle='-', linewidth=ln_width, color='black', label='Interpolated Entropy')
+    plt.axvline(x=1000, linestyle='--', linewidth=ln_width, color='gray', label='Length 1000')
     plt.xlabel('Sequence Length')
     plt.ylabel('Expected Entropy')
     plt.title(composer.capitalize())
@@ -115,5 +115,53 @@ def composer_entropy(composer: str, instruments: [str]) -> (float, float):
     return interp(1000), reference_entropy(len(vocabulary), 1000)
 
 
+def main():
+    print(composer_entropy(sys.argv[1], sys.argv[2:]))
+    crt_dir = get_dir(sys.argv[1], sys.argv[2:])
+    no_trials = 10
+    thresholds = [0.10, 0.25, 0.33, 0.50]
+    for threshold in thresholds:
+        with open(os.path.join(crt_dir, 'results.csv'), 'rt') as report:
+            report.readline()
+            xs = []
+            ys_wo = []
+            ys_w = []
+            ys_ref = []
+            ys_nz = []
+            for line in report.readlines():
+                line = line.rstrip('\n')
+                tokens = line.split(',')
+                motif_threshold = float(tokens[0].strip(' '))
+                sampling_temperature = float(tokens[1].strip(' '))
+                expected_entropy = float(tokens[2].strip(' '))
+                noise_entropy = float(tokens[3].strip(' '))
+                without_motifs = [float(token.strip(' ')) for token in tokens[4:14]]
+                with_motifs = [float(token.strip(' ')) for token in tokens[14:24]]
+
+                if motif_threshold == threshold:
+                    xs.append(sampling_temperature)
+                    ys_wo.append(np.mean(without_motifs))
+                    ys_w.append(np.mean(with_motifs))
+                    ys_ref.append(expected_entropy)
+                    ys_nz.append(noise_entropy)
+
+            pth_plot = os.path.join(crt_dir, f'entropy_plot_motifs_thr_{threshold:.2f}.png')
+            dpi = 72
+            fig_width = 1000
+            fig_height = 500
+            ln_width = 2
+            plt.figure(figsize=(fig_width / dpi, fig_height / dpi), dpi=dpi)
+            plt.plot(xs, ys_wo, linestyle='-', linewidth=ln_width, color='darkgray', label='Without Motifs')
+            plt.plot(xs, ys_w, linestyle='-', linewidth=ln_width, color='lightgray', label='With Motifs')
+            plt.plot(xs, ys_ref, linestyle='--', linewidth=ln_width, color='gray', label='Expected Entropy')
+            plt.plot(xs, ys_nz, linestyle=':', linewidth=ln_width, color='gray', label='Noise Entropy')
+            plt.xlabel('Sampling Temperature')
+            plt.ylabel(f'Avg. Entropy ({no_trials} Trials)')
+            plt.title(sys.argv[1].capitalize())
+            plt.legend()
+            plt.savefig(pth_plot, dpi=dpi, bbox_inches='tight')
+            plt.close('all')
+
+
 if __name__ == "__main__":
-    composer_entropy(sys.argv[1], sys.argv[2:])
+    main()
